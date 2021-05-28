58ebe38bd114fdd9e17295435ac2e057a8c4eb34
Dalton Willard <willarddalton@gmail.com>
2021-05-27 21:40:39 -0600

    Director initialization method created

--- mastermind/game/director.py
+++ mastermind/game/director.py
@@ -28,7 +28,15 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        
+        self.__keep_playing = True
+
+        self.__board = Board()
+        self.__console = Console()
+        self.__helper = Helper()
+        self.__move = Move()
+        self.__player = Player()
+        self.__roster = Roster()
+
     def start_game(self):
         """Starts the game loop to control the sequence of play.
         
