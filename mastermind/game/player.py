3b971524260d52c522a84ccd380235aeb77b8396
Kelton Green <greenkelton@protonmail.com>
2021-05-27 21:15:54 -0600

    player class

--- mastermind/game/player.py
+++ mastermind/game/player.py
@@ -1,5 +1,7 @@
 class Player:
-    """A person taking part in a game. The responsibility of Player is to keep track of their identity and last move.
+    """A person taking part in a game. 
+       The responsibility of Player is to keep track of their identity 
+       and last move.
     
     Stereotype: 
         Information Holder
