5cccd68fe0dfb2be1ba31bb006de1b54685ad069
Dalton Willard <willarddalton@gmail.com>
2021-05-27 21:21:06 -0600

    Initial creation of stubs and placeholders

---
+++ mastermind/game/console.py
@@ -0,0 +1,25 @@
+class Console:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Service Provider, Interfacer
+
+    Attributes:
+        _method (type): Description of method goes here.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Console): an instance of Console.
+        """
+        
+    def some_method(self):
+        """Description of what the method does.
+        
+        Args:
+            self (Console): an instance of Console.
+        """
+        pass
